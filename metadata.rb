1595177fbeb5f84b6a5f7f4e562931a029d96515
Ian Mulvany <ian@mulvany.net>
2013-08-07 13:10:14 +0100

    inital blank cookbook

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'elife-drupal-local-env'
+maintainer       'Ian Mulvany'
+maintainer_email 'i.mulvany@elifesciences.org'
+license          'All rights reserved'
+description      'Installs/Configures elife-drupal-local-env'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
