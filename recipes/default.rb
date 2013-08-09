8c77a8774cff914e54ba808d640c8ac14f515358
Ian Mulvany <ian@mulvany.net>
2013-08-09 10:33:40 +0100

    Added a block to prevent the vm from requiring ssh to check for known hosts.
    Added a block to attempt to get a private repo
    
    Access to the private repo should be provided via ssh agent forwarding from the host machine

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,42 @@
 # All rights reserved - Do Not Redistribute
 #
 
+# add a .ssh directory
+Directory "/root/.ssh" do
+  action :create
+  mode 0700
+end
+
+# don't check for hosts
+File "/root/.ssh/config" do
+  action :create
+  content "Host *\nStrictHostKeyChecking no"
+  mode 0600
+end
+
+ruby_block "Give root access to the forwarded ssh agent" do
+  block do
+    # find a parent process' ssh agent socket
+    agents = {}
+    ppid = Process.ppid
+    Dir.glob('/tmp/ssh*/agent*').each do |fn|
+      agents[fn.match(/agent\.(\d+)$/)[1]] = fn
+    end
+    while ppid != '1'
+      if (agent = agents[ppid])
+        ENV['SSH_AUTH_SOCK'] = agent
+        break
+      end
+      File.open("/proc/#{ppid}/status", "r") do |file|
+        ppid = file.read().match(/PPid:\s+(\d+)/)[1]
+      end
+    end
+    # Uncomment to require that an ssh-agent be available
+    # fail "Could not find running ssh agent - Is config.ssh.forward_agent enabled in Vagrantfile?" unless ENV['SSH_AUTH_SOCK']
+  end
+  action :create
+end
+
 include_recipe "git"
 
 # where to put our repositories
@@ -19,14 +55,10 @@ directory localgit do
   action :create
 end
 
-# install git repos to our local path
+# prove that we can add a couple of public repositories
 git_repos = Array.new
 git_repos.push ["git://github.com/elifesciences/", "elife-api-prototype"]
 git_repos.push ["git://github.com/elifesciences/", "elife-bot"]
-git_repos.push ["git://github.com/elifesciences/", "drupal-site-jnl-elife"]
-#git_repos.push ["git@github.com:highwire/", "drupal-highwire"]
-#git_repos.push ["git@github.com:highwire/", "drupal-webroot"]
-
 
 git_repos.each do |repos|
   base_uri = repos[0]
@@ -39,3 +71,17 @@ git_repos.each do |repos|
     action :sync
   end
 end
+
+# add elife version of drupal jnl site
+elife_jnl_dev_uri = "git@github.com:elifesciences/drupal-site-jnl-elife.git"
+repos_dir_jnl = "/home/vagrant/localgit/drupal-site-jnl-elife"
+git repos_dir_jnl do
+  repository elife_jnl_dev_uri
+  reference "elife-dev"
+  action :sync
+end 
+
+# attempt to add highwire core drupal repositories
+#git_repos.push ["git@github.com:highwire/", "drupal-highwire"]
+#git_repos.push ["git@github.com:highwire/", "drupal-webroot"]
+
