4639593717e5ecc160a23a44be10ff5de56f0537
Ruth Ivimey-Cook <r.ivimeycook@elifesciences.org>
2013-12-04 15:53:36 +0000

    Add attributes file for default node values for :elifejnl params

---
+++ attributes/default.rb
@@ -0,0 +1,8 @@
+#
+# Some vaguely sane defaults for elifejnl values.
+#
+
+default[:elifejnl][:setup_vpn_client] = false
+default[:elifejnl][:hiwire_rev] = "7.x-1.x"
+default[:elifejnl][:webroot_rev] = "7.x-1.x"
+
